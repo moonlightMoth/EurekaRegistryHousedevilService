47fa56e680fee298192e6d3a091f505805f167ff
moonlightMoth <jyhgftm@gmail.com>
2022-05-19 01:09:47 +0300

    Initial commit

---
+++ src/test/java/moonlightmoth/housedevil/eurekaregistry/EurekaregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package moonlightmoth.housedevil.eurekaregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
